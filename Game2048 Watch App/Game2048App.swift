1eee14906baaf9f340d8950c380b78273a649bb8
CEOShow <showhihorse@gmail.com>
2025-02-22 20:10:39 +0800

    顏色正確，不過沒有效果，有一些邏輯不對，有的時候合不起來

---
+++ Game2048 Watch App/Game2048App.swift
@@ -0,0 +1,18 @@
+//
+//  Game2048App.swift
+//  Game2048 Watch App
+//
+//  Created by Show on 2025/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
+
